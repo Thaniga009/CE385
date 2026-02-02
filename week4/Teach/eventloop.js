e5add1c7b1511c6820018ec45c1a0bdbed37e21c
Thaniga009 <th4nigaaom@gmail.com>
2026-02-02 15:46:13 +0700

    get

---
+++ week4/Teach/eventloop.js
@@ -0,0 +1,20 @@
+const eventloop = (callback) => {
+    console.log("Begin");
+    setTimeout(() => {
+        const result = callback("First");
+        console.log(result);
+    }, 2000);
+    setTimeout(() => {
+        const result = callback("Second");
+         console.log(result);
+    }, 1000);
+    setTimeout(() => {
+        const result = callback("Third");
+         console.log(result);
+    }, 0);
+    console.log("end");
+
+}
+eventloop((message) => {
+    return message;
+});
--- week4/test.js
+++
@@ -1,3 +0,0 @@
-let a= 13;
-let v= 15 ;
-console.log("num:",a);
